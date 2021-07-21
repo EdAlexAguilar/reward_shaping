c65d2009791bee190b548fbf780adf355a82dd81
Axel Brunnbauer <axel.brunnbauer@gmx.at>
2021-07-21 13:25:29 +0200

    fix graph based reward.

--- reward_shaping/envs/graph_based.py
+++ reward_shaping/envs/graph_based.py
@@ -1,4 +1,5 @@
 import pathlib
+from typing import Dict, List
 
 import numpy as np
 import yaml
@@ -13,8 +14,17 @@ from reward_shaping.envs.core import RewardFunction, RewardWrapper
 
 class GraphBasedReward(RewardFunction):
 
-    def __init__(self, reward_of_interest: str):
+    def __init__(self, nodes: Dict[str, RewardFunction] = None, topology: Dict[str, List[str]] = None):
         self._graph = nx.DiGraph()
+        if topology and not nodes:
+            raise ValueError('If a topology is provided, nodes must also be provided.')
+        if nodes:
+            for node, reward_fn in nodes.items():
+                self.add_reward(node, reward_fn)
+        if topology:
+            self._graph.add_edges_from(topology)
+            if not nx.is_directed_acyclic_graph(self._graph):
+                raise ValueError('Graph is not a DAG.')
 
     def add_reward(self, label: str, reward_fn: RewardFunction):
         self._graph.add_node(label, reward_fn=reward_fn)
@@ -63,32 +73,35 @@ class GraphBasedReward(RewardFunction):
         return reward
 
 
-
-if __name__ == '__main__':
-    graph = GraphBasedReward(reward_of_interest='T_bal')
-    graph.add_reward('S_coll', reward_fn=reward_fns.CollisionReward(no_collision_bonus=10))
-    graph.add_reward('S_fall', reward_fn=reward_fns.ContinuousFalldownReward(theta_limit=5))
-    graph.add_reward('H_nfeas', reward_fn=reward_fns.Indicator(
-        reward_fns.CheckOvercomingFeasibility(obstacle_y=1, axle_y=1, feasible_height=5), negate=True))
-    graph.add_reward('H_feas', reward_fn=reward_fns.Indicator(
-        reward_fns.CheckOvercomingFeasibility(obstacle_y=1, axle_y=1, feasible_height=5)))
-    graph.add_reward('T_orig', reward_fn=reward_fns.ReachTargetReward(x_target=0, x_target_tol=2))
-    graph.add_reward('T_bal', reward_fn=reward_fns.BalanceReward(theta_target=0, theta_target_tol=0.1))
-
-    graph.add_dependency(source='S_coll', target='H_feas')
-    graph.add_dependency(source='S_coll', target='H_nfeas')
-    graph.add_dependency(source='S_fall', target='H_feas')
-    graph.add_dependency(source='S_fall', target='H_nfeas')
-    graph.add_dependency(source='H_feas', target='T_orig')
-    graph.add_dependency(source='H_nfeas', target='T_bal')
-    #graph.add_dependency(source='T_orig', target='T_bal')
-
+def make_env():
+    global task, env
     task = "random_height"
     env_config = pathlib.Path(f"cart_pole_obst/tasks/{task}.yml")
     with open(env_config, 'r') as file:
         env_params = yaml.load(file, yaml.FullLoader)
     env = CartPoleContObsEnv(**env_params, eval=True, seed=0)
-    env = RewardWrapper(env, reward_fn=graph)
+    return env
+
+if __name__ == '__main__':
+    rewards = {
+        'S_coll': reward_fns.CollisionReward(no_collision_bonus=10),
+        'S_fall': reward_fns.ContinuousFalldownReward(theta_limit=5),
+        'H_nfeas': reward_fns.Indicator(reward_fns.CheckOvercomingFeasibility(obstacle_y=1, axle_y=1, feasible_height=5), negate=True),
+        'H_feas': reward_fns.Indicator(reward_fns.CheckOvercomingFeasibility(obstacle_y=1, axle_y=1, feasible_height=5)),
+        'T_orig': reward_fns.ReachTargetReward(x_target=0, x_target_tol=2),
+        'T_bal': reward_fns.BalanceReward(theta_target=0, theta_target_tol=0.1)
+    }
+    topology = {
+        'S_coll': ['H_feas', 'H_nfeas'],
+        'S_fall': ['H_feas', 'H_nfeas'],
+        'H_feas': ['T_orig'],
+        'H_nfeas': ['T_bal'],
+        'T_orig': ['T_bal'],
+    }
+
+    graph_reward = GraphBasedReward(nodes=rewards, topology=topology)
+    env = make_env()
+    env = RewardWrapper(env, reward_fn=graph_reward)
     env.reset()
     for x in range(1000000):
         obs, reward, done, info = env.step(np.array([0]))
