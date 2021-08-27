1437ad32da1df1696eec8f9de46571f2602f7d07
luigiberducci <luigi.berd@gmail.com>
2021-08-27 18:43:48 +0200

    prepare docker image for experiments

---
+++ entrypoint.sh
@@ -0,0 +1,1 @@
+xvfb-run -s "-screen 0 1400x900x24" python run_training.py
--- run_training.py
+++ run_training.py
@@ -5,11 +5,11 @@ from reward_shaping.training.train import train
 
 def main(args):
     train_params = {'steps': args.steps,
-                    'video_every': args.video_every,
-                    'n_recorded_episodes': args.video_episodes,
-                    'eval_every': args.eval_every,
-                    'n_eval_episodes': args.eval_episodes,
-                    'checkpoint_every': args.checkpoint_every}
+                    'video_every': int(args.steps/10),
+                    'n_recorded_episodes': 5,
+                    'eval_every': int(args.steps/10),
+                    'n_eval_episodes': 10,
+                    'checkpoint_every': int(args.steps/10)}
     train(args.env, args.task, args.reward, train_params, seed=args.seed)
 
 
@@ -20,11 +20,6 @@ if __name__ == "__main__":
     parser.add_argument("--task", type=str, required=True)
     parser.add_argument("--reward", type=str, required=True)
     parser.add_argument("--steps", type=int, default=1e6)
-    parser.add_argument("--eval_every", type=int, default=1e5)
-    parser.add_argument("--eval_episodes", type=int, default=10)
-    parser.add_argument("--video_every", type=int, default=1e5)
-    parser.add_argument("--video_episodes", type=int, default=5)
-    parser.add_argument("--checkpoint_every", type=int, default=1e5)
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
     main(args)
