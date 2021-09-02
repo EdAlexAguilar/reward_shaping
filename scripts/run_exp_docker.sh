20cddb8b3c17259c240409f2a25701cbeeea8b2d
luigiberducci <luigi.berd@gmail.com>
2021-09-02 11:10:23 +0200

    update experiment scripts to take training algo as input

--- scripts/run_exp_docker.sh
+++ scripts/run_exp_docker.sh
@@ -2,18 +2,20 @@
 
 env=$1
 task=$2
-expdir=$3
-reward=$4
+algo=$3
+expdir=$4
+reward=$5
+
 image=luigi/reward_shaping
 gpus=all
 
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then 
-	echo "illegal number of params. help: $0 <env> <task> <exp_dir> <reward> "
+	echo "illegal number of params. help: $0 <env> <task> <algo> <exp_dir> <reward> "
 	exit -1
 fi
 
 docker run --rm -it \
 	       -u $(id -u):$(id -g) -v $(pwd):/src \
 	       --gpus $gpus $image \
-	       /bin/bash entrypoint.sh $env $task $expdir $reward
+	       /bin/bash entrypoint.sh $env $task $algo $expdir $reward
