8f8e7ce371df38f7b9af096f28860805b20fe988
Luigi Berducci <luigi.berd@gmail.com>
2022-01-27 10:58:24 +0100

    fix kill_all script

--- scripts/kill_all.sh
+++ scripts/kill_all.sh
@@ -9,16 +9,14 @@ do
     # first try to kill single-instance experiment
     docker kill $c_name &> /dev/null && echo "[$debug_prefix] Killed: $c_name"
   } || {
-    {
     # if failed, then try to kill instances of (sequential batch) experiment
     for j in {1..10}
     do
-      c_name="exp_$i_$j"
-      docker kill $c_name &> /dev/null && echo "[$debug_prefix] Killed: $c_name"
+      c_sub_name="exp_${i}_${j}"
+      docker kill $c_sub_name &> /dev/null && echo "[$debug_prefix] Killed: $c_sub_name"
     done
-    } || {
+  } || {
       # if failed again, then assume not existing container
-      echo "[$debug_prefix] Failed to kill: $c_name"
-    }
+      echo "[$debug_prefix] Failed to kill: ${c_name}_[1-10]"
   }
 done
