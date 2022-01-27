140cc6fe18b2b6330629699abbdd429f3a0a21df
Luigi Berducci <luigi.berd@gmail.com>
2022-01-27 15:02:22 +0100

    run docker deamon

--- scripts/run_n_exps_docker.sh
+++ scripts/run_n_exps_docker.sh
@@ -28,7 +28,7 @@ echo "[$debug_prefix] Running ${expname}: repeat ${n_seeds} seeds"
 
 for i in `seq 1 $n_seeds`
 do
-	docker run --rm -it --name $expname \
+	docker run --rm -it --name $expname -d \
                -u $(id -u):$(id -g) -v $(pwd):/src \
                --gpus $gpus $image \
                /bin/bash entrypoint.sh $env $task $algo $expdir $reward $steps $n_seeds
