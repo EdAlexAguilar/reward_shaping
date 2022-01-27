d889db8a34e295826bd8f526c2e8a21d564bb6b4
Luigi Berducci <luigi.berd@gmail.com>
2022-01-27 10:49:43 +0100

    implement kill_all for sub-isntances of seq experiment

--- scripts/kill_all.sh
+++ scripts/kill_all.sh
@@ -6,8 +6,16 @@ for i in {1..16}
 do
   c_name="exp_$i"
   {
+    # first try to kill single-instance experiment
     docker kill $c_name &> /dev/null && echo "[$debug_prefix] Killed: $c_name"
   } || {
+    # if failed, then try to kill instances of (sequential batch) experiment
+    for j in {1..10}
+    do
+      c_name="exp_$i_$j"
+      docker kill $c_name &> /dev/null && echo "[$debug_prefix] Killed: $c_name"
+  } || {
+    # if failed again, then assume not existing container
     echo "[$debug_prefix] Failed to kill: $c_name"
   }
 done
