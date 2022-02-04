100c4f46b3388b1d63d51a73f75a560ba1ffd1e3
Luigi Berducci <luigi.berd@gmail.com>
2022-02-04 17:45:46 +0100

    simplify kill_all

--- scripts/kill_all.sh
+++ scripts/kill_all.sh
@@ -5,15 +5,6 @@ debug_prefix="kill_all"
 for i in {1..30}
 do
   c_name="exp_$i"
-  {
-    # first try to kill single-instance experiment
-    docker kill $c_name &> /dev/null && echo "[$debug_prefix] Killed: $c_name"
-  } || {
-    # if failed, then try to kill instances of (sequential batch) experiment
-    for j in {1..10}
-    do
-      c_sub_name="exp_${i}_${j}"
-      docker kill $c_sub_name &> /dev/null && echo "[$debug_prefix] Killed: $c_sub_name"
-    done
-  }
+  # first try to kill single-instance experiment
+  docker kill $c_name &> /dev/null && echo "[$debug_prefix] Killed: $c_name"
 done
