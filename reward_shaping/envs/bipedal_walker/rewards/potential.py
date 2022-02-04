bugifx: correct computation of hierarchical potential

reward_shaping/envs/bipedal_walker/rewards/potential.py
@@ -12,7 +12,7 @@ def safety_collision_potential(state, info):
     return int(state["collision"] <= 0)
 
 
-def target_potential(state, info):
+def dist_to_target(state, info):
     assert "x" in state
     return np.clip(state["x"], 0.0, 1.0)  # already normalize, safety clipping to avoid unexpected values
 
@@ -54,8 +54,8 @@ class BWHierarchicalPotentialShaping(RewardFunction):
         return safety_collision_potential(state, info)
 
     def _target_potential(self, state, info):
-        safety_w = self._safety_potential(state, info)
-        return safety_w * target_potential(state, info)
+        safety_w = safety_collision_potential(state, info)
+        return safety_w * dist_to_target(state, info)
 
     def _comfort_potential(self, state, info):
         comfort_vx = comfort_vx_potential(state, info)
@@ -63,7 +63,8 @@ class BWHierarchicalPotentialShaping(RewardFunction):
         comf_vy = comfort_vy_potential(state, info)
         comf_angle_vel = comfort_ang_vel_potential(state, info)
         # hierarchical weights
-        safety_w, target_w = self._safety_potential(state, info), self._target_potential(state, info)
+        safety_w = safety_collision_potential(state, info)
+        target_w = dist_to_target(state, info)
         return safety_w * target_w * (comfort_vx + comf_vy + comf_angle + comf_angle_vel)
 
     def __call__(self, state, action=None, next_state=None, info=None) -> float:
@@ -91,7 +92,7 @@ class BWScalarizedMultiObjectivization(RewardFunction):
             return base_reward
         # evaluate individual shaping functions
         shaping_coll = safety_collision_potential(next_state, info) - safety_collision_potential(state, info)
-        shaping_target = target_potential(next_state, info) - target_potential(state, info)
+        shaping_target = dist_to_target(next_state, info) - dist_to_target(state, info)
         shaping_comf_vx = comfort_vx_potential(next_state, info) - comfort_vx_potential(state, info)
         shaping_comf_vy = comfort_vy_potential(next_state, info) - comfort_vy_potential(state, info)
         shaping_comf_ang = comfort_angle_potential(next_state, info) - comfort_angle_potential(state, info)

reward_shaping/envs/f1tenth/core/single_agent_env.py
@@ -89,6 +89,7 @@ class SingleAgentRaceEnv(F110Env):
                       "velocity": gym.spaces.Box(low=-5.0, high=20.0, shape=(1,)),
                       "steering": gym.spaces.Box(low=-0.4189, high=0.4189, shape=(1,)),
                       "collision": gym.spaces.Box(low=0.0, high=1.0, shape=(1,)),
+                      "reverse": gym.spaces.Box(low=0.0, high=1.0, shape=(1,)),
                       "progress": gym.spaces.Box(low=0.0, high=1.0, shape=(1,)),
                       "progress_meters": gym.spaces.Box(low=0.0, high=self.track.track_length, shape=(1,)),
                       "lane": gym.spaces.Box(low=0.0, high=1.0, shape=(1,)),
@@ -133,7 +134,8 @@ class SingleAgentRaceEnv(F110Env):
 
     def process_obs_conf(self, obs_conf):
         default = {
-            'types': ['scan', 'pose', 'velocity', 'lidar_occupancy', 'steering', 'progress', 'progress_meters', 'collision', 'lane'],
+            'types': ['scan', 'pose', 'velocity', 'lidar_occupancy', 'steering', 'progress', 'progress_meters',
+                      'collision', 'reverse', 'lane'],
             'max_range': 10.0, 'resolution': 0.25, 'frame_skip': 4, 'degree_fov': 360}
         return self._process_conf(default, obs_conf)
 
@@ -177,7 +179,8 @@ class SingleAgentRaceEnv(F110Env):
                    'progress': self.progress,
                    'progress_meters': self.progress * self.track.track_length,
                    'lane': self._track.get_lane(np.array([old_obs['poses_x'][0], old_obs['poses_y'][0]])),
-                   'collision': old_obs['collisions'][0]}
+                   'collision': old_obs['collisions'][0],
+                   'reverse': self.reverse}
         filtered_obs = {}
         for obs, value in all_obs.items():
             if obs not in self.observations_conf["types"]:
@@ -194,11 +197,11 @@ class SingleAgentRaceEnv(F110Env):
         # target_progress := the space that the car can cover with the comfortable speed (or the max track len for shorter tracks)
         target_progress_mt = min(self.track.track_length,
                                  self.timestep * self.termination_conf["max_steps"] * (
-                                             self.comf_speed_min + (self.comf_speed_max - self.comf_speed_min) / 2))
+                                         self.comf_speed_min + (self.comf_speed_max - self.comf_speed_min) / 2))
         info = {'checkpoint_done': old_info['checkpoint_done'][0],
                 'lap_time': old_obs['lap_times'][0],
                 'lap_count': old_obs['lap_counts'][0],
-                'collision': old_obs['collisions'][0],
+                'collision': bool(old_obs['collisions'][0]),
                 'velocity': old_obs['linear_vels_x'][0],
                 'time': self._step * self.timestep,
                 'progress': self.progress,

reward_shaping/envs/f1tenth/rewards/baselines.py
@@ -26,18 +26,19 @@ class F110EvalConfig(EvalConfig):
 
     @property
     def monitoring_variables(self):
-        return ['time', 'collision', 'progress', 'velocity', 'steering', 'lane',
+        return ['time', 'collision', 'reverse', 'progress', 'velocity', 'steering', 'lane',
                 'comfortable_steering', 'comfortable_speed_min', 'comfortable_speed_max', 'favourite_lane']
 
     @property
     def monitoring_types(self):
-        return ['int', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float']
+        return ['int', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float', 'float']
 
     def get_monitored_state(self, state, done, info) -> Dict[str, Any]:
         # compute monitoring variables (all of them normalized in 0,1)
         monitored_state = {
             'time': info['time'],
             'collision': state['collision'],  # already 0 or 1
+            'reverse': state['reverse'],  # already 0 or 1
             'progress': state['progress'],
             'velocity': state['velocity'],
             'steering': state['steering'],
@@ -55,7 +56,7 @@ class F110EvalConfig(EvalConfig):
         i_init = np.nonzero(episode['time'] == np.min(episode['time']))[-1][-1]
         episode = {k: list(l)[i_init:] for k, l in episode.items()}
         #
-        safety_spec = "always(collision<=0)"
+        safety_spec = "always((collision<=0) and (reverse <= 0))"
         safety_rho = monitor_episode(stl_spec=safety_spec,
                                      vars=self.monitoring_variables, types=self.monitoring_types,
                                      episode=episode)[0][1]