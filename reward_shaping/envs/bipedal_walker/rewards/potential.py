fix weights normalization in morl baseline, adjust dependencies dockerfile f110, check gamma parameter

reward_shaping/envs/bipedal_walker/rewards/potential.py
@@ -111,7 +111,8 @@ class BWScalarizedMultiObjectivization(RewardFunction):
 class BWUniformScalarizedMultiObjectivization(BWScalarizedMultiObjectivization):
 
     def __init__(self, **kwargs):
-        weights = [0.167, 0.167, 0.167, 0.167, 0.167,  0.165]   # 0.165 to have sum = 1.0
+        weights = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+        weights /= np.sum(weights)
         super(BWUniformScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)
 
 
@@ -123,5 +124,6 @@ class BWDecreasingScalarizedMultiObjectivization(BWScalarizedMultiObjectivizatio
         - the sum of comfort weights is ~ 0.25/1.75
     """
     def __init__(self, **kwargs):
-        weights = [0.56, 0.28, 0.04, 0.04, 0.04,  0.04]
+        weights = np.array([1.0, 0.5, 0.25, 0.25, 0.25, 0.25])
+        weights /= np.sum(weights)
         super(BWDecreasingScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)

reward_shaping/envs/cart_pole_obst/rewards/potential.py
@@ -6,7 +6,7 @@ import numpy as np
 from reward_shaping.core.utils import clip_and_norm
 from reward_shaping.envs.cart_pole_obst.specs import get_all_specs
 
-gamma = 0.99
+gamma = 1.0
 
 def safety_falldown_potential(state, info):
     assert "theta" in state and "theta_limit" in info
@@ -124,7 +124,8 @@ class CPOScalarizedMultiObjectivization(RewardFunction):
 class CPOUniformScalarizedMultiObjectivization(CPOScalarizedMultiObjectivization):
 
     def __init__(self, **kwargs):
-        weights = [0.2, 0.2, 0.2, 0.2, 0.2]
+        weights = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
+        weights /= np.sum(weights)
         super(CPOUniformScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)
 
 
@@ -137,5 +138,6 @@ class CPODecreasingScalarizedMultiObjectivization(CPOScalarizedMultiObjectivizat
             - the sum of target weights is ~ 0.50/1.75
             - the sum of comfort weights is ~ 0.25/1.75
         """
-        weights = [0.19, 0.19, 0.19, 0.28, 0.15]
+        weights = np.array([1.0, 1.0, 1.0, 0.5, 0.25])
+        weights /= np.sum(weights)
         super(CPODecreasingScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)

reward_shaping/envs/f1tenth/rewards/potential.py
@@ -6,7 +6,8 @@ from reward_shaping.core.reward import RewardFunction
 from reward_shaping.core.utils import clip_and_norm
 from reward_shaping.envs.f1tenth.specs import get_all_specs
 
-gamma = 0.99
+gamma = 1.0
+
 
 def safety_collision_potential(state, info):
     assert "collision" in state
@@ -25,7 +26,8 @@ def target_potential(state, info):
 
 def comfort_speed_potential(state, info):
     assert "velocity" in state and "comfortable_speed_limit" in info
-    return 1.0 - clip_and_norm(state["velocity"][0], info["comfortable_speed_limit"], info["max_speed"])  # 0 > threshold
+    return 1.0 - clip_and_norm(state["velocity"][0], info["comfortable_speed_limit"],
+                               info["max_speed"])  # 0 > threshold
 
 
 def comfort_steering_potential(state, info):
@@ -90,14 +92,16 @@ class F110ScalarizedMultiObjectivization(RewardFunction):
             return base_reward
         # evaluate individual shaping functions
         shaping_coll = gamma * safety_collision_potential(next_state, info) - safety_collision_potential(state, info)
+        shaping_reverse = gamma * safety_reverse_potential(next_state, info) - safety_reverse_potential(state, info)
         shaping_target = gamma * target_potential(next_state, info) - target_potential(state, info)
         shaping_comf_speed = gamma * comfort_speed_potential(next_state, info) - comfort_speed_potential(state, info)
-        shaping_comf_steer = gamma * comfort_steering_potential(next_state, info) - comfort_steering_potential(state, info)
+        shaping_comf_steer = gamma * comfort_steering_potential(next_state, info) - comfort_steering_potential(state,
+                                                                                                               info)
         shaping_comf_lane = gamma * comfort_lane_potential(next_state, info) - comfort_lane_potential(state, info)
         # linear scalarization of the multi-objectivized requirements
         reward = base_reward
         for w, f in zip(self._weights,
-                        [shaping_coll, shaping_target, shaping_comf_speed, shaping_comf_steer, shaping_comf_lane]):
+                        [shaping_coll, shaping_reverse, shaping_target, shaping_comf_speed, shaping_comf_steer, shaping_comf_lane]):
             reward += w * f
         return reward
 
@@ -105,7 +109,8 @@ class F110ScalarizedMultiObjectivization(RewardFunction):
 class F110UniformScalarizedMultiObjectivization(F110ScalarizedMultiObjectivization):
 
     def __init__(self, **kwargs):
-        weights = [0.2, 0.2, 0.2, 0.2, 0.2]  # 0.2 to have sum = 1.0
+        weights = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+        weights /= np.sum(weights)
         super(F110UniformScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)
 
 
@@ -118,5 +123,6 @@ class F110DecreasingScalarizedMultiObjectivization(F110ScalarizedMultiObjectiviz
     """
 
     def __init__(self, **kwargs):
-        weights = [0.56, 0.29, 0.05, 0.05, 0.05]
+        weights = np.array([1.0, 1.0, 0.5, 0.25, 0.25, 0.25])
+        weights /= np.sum(weights)
         super(F110DecreasingScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)

reward_shaping/envs/lunar_lander/rewards/potential.py
@@ -7,7 +7,7 @@ from reward_shaping.core.utils import clip_and_norm
 from reward_shaping.envs.lunar_lander.specs import get_all_specs
 
 
-gamma = 0.99
+gamma = 1.0
 
 def safety_collision_potential(state, info):
     assert "collision" in state
@@ -96,7 +96,8 @@ class LLScalarizedMultiObjectivization(RewardFunction):
 class LLUniformScalarizedMultiObjectivization(LLScalarizedMultiObjectivization):
 
     def __init__(self, **kwargs):
-        weights = [0.2, 0.2, 0.2, 0.2, 0.2]
+        weights = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
+        weights /= np.sum(weights)
         super(LLUniformScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)
 
 
@@ -109,5 +110,6 @@ class LLDecreasingScalarizedMultiObjectivization(LLScalarizedMultiObjectivizatio
             - the sum of target weights is ~ 0.50/1.75
             - the sum of comfort weights is ~ 0.25/1.75
         """
-        weights = [0.29, 0.29, 0.28, 0.07, 0.07]
+        weights = np.array([1.0, 1.0, 0.5, 0.25, 0.25])
+        weights /= np.sum(weights)
         super(LLDecreasingScalarizedMultiObjectivization, self).__init__(weights=weights, **kwargs)