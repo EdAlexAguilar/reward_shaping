39c0f42ebc5a2835a82a737e28d95cd03eb4a795
luigiberducci <luigi.berd@gmail.com>
2022-02-06 15:19:55 +0100

    add nsteps in debug mode

--- scripts/slurm_exps.sh
+++ scripts/slurm_exps.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 #SBATCH -J array
 #SBATCH -N 1
-#SBATCH --array=1-31
-#SBATCH --output=../logs/slurm-%j-%A-%a.out
-#SBATCH --error=../logs/slurm-%j-%A-%a.err
+#SBATCH --array=1-6
+#SBATCH --output=${dir}/logs/slurm-%j-%A-%a.out
+#SBATCH --error=${dir}/logs/slurm-%j-%A-%a.err
 
 module purge
 module load singularity
 
 source ./init_exp_list.sh   # load list of args
 
-echo "args: "  ${args[$SLURM_ARRAY_TASK_ID]}
-echo ""
+aa=${args[$SLURM_ARRAY_TASK_ID]}
+image="../reward_shaping.sif"
+
+echo $aa
