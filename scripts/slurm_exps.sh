b01fea6ce15efa199021d2f70befd9f36e4b4bb2
luigiberducci <luigi.berd@gmail.com>
2022-02-06 15:29:38 +0100

    working on slurm

--- scripts/slurm_exps.sh
+++ scripts/slurm_exps.sh
@@ -9,8 +9,6 @@
 module purge
 module load go/1.13.15 singularity/3.8.3
 
-source ./init_exp_list.sh   # load list of args
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 aa=${args[$SLURM_ARRAY_TASK_ID]}
 image="${DIR}/../reward_shaping.sif"
@@ -18,3 +16,5 @@ image="${DIR}/../reward_shaping.sif"
 echo "Dir: ${DIR}"
 echo "Image SIF: ${image}"
 echo "ARGS: ${aa}"
+
+source ${DIR}/init_exp_list.sh   # load list of args
