507db65a4ed639e9c5a1976e8b73fa5ad52fec73
luigiberducci <luigi.berd@gmail.com>
2022-02-06 15:28:35 +0100

    working on slurm

--- scripts/slurm_exps.sh
+++ scripts/slurm_exps.sh
@@ -3,17 +3,18 @@
 #SBATCH -J array
 #SBATCH -N 1
 #SBATCH --array=1-6
-#SBATCH --output=logs/slurm-%j-%A-%a.out
-#SBATCH --error=logs/slurm-%j-%A-%a.err
+#SBATCH --output=slurm-%j-%A-%a.out
+#SBATCH --error=slurm-%j-%A-%a.err
 
 module purge
 module load go/1.13.15 singularity/3.8.3
 
-mkdir -p logs
 source ./init_exp_list.sh   # load list of args
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 aa=${args[$SLURM_ARRAY_TASK_ID]}
 image="${DIR}/../reward_shaping.sif"
 
-echo $aa
+echo "Dir: ${DIR}"
+echo "Image SIF: ${image}"
+echo "ARGS: ${aa}"
