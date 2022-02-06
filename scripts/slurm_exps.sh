3683060e3591ebe293ff305ec0d766687fc367db
luigiberducci <luigi.berd@gmail.com>
2022-02-06 14:48:56 +0100

    init slurm scripts

--- scripts/slurm_exps.sh
+++ scripts/slurm_exps.sh
@@ -2,8 +2,8 @@
 #SBATCH -J array
 #SBATCH -N 1
 #SBATCH --array=1-31
-#SBATCH --output=../logs/slurm-%x.%j.out
-#SBATCH --error=../logs/slurm-%x.%j.err
+#SBATCH --output=../logs/slurm-%j-%A-%a.out
+#SBATCH --error=../logs/slurm-%j-%A-%a.err
 
 source ./init_exp_list.sh   # load list of args
 
