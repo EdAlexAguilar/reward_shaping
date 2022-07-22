fd9ece254ca582326d18d5d7f580c01ee1bb76f4
luigiberducci <luigi.berd@gmail.com>
2022-07-22 16:12:24 +0200

    specs: define bounds on each comfort requirements, implement stl eval

--- reward_shaping/envs/racecar/single_agent_racecar_env.py
+++ reward_shaping/envs/racecar/single_agent_racecar_env.py
@@ -11,7 +11,8 @@ from racecar_gym import SingleAgentScenario
 from racecar_gym.envs.gym_api import ChangingTrackSingleAgentRaceEnv
 from gym.utils import seeding
 
-from reward_shaping.envs.wrappers import DeltaSpeedWrapper
+from reward_shaping.envs.racecar.wrappers import DeltaSpeedWrapper
+from reward_shaping.envs.racecar.wrappers.racecar_wrappers import ActionHistoryWrapper, ObservationHistoryWrapper
 
 
 class RacecarEnv(ChangingTrackSingleAgentRaceEnv):
@@ -28,14 +29,15 @@ class RacecarEnv(ChangingTrackSingleAgentRaceEnv):
         # spec params
         self._target_progress = params["reward_params"]["target_progress"]
         self._target_dist2obst = params["reward_params"]["target_dist2obst"]
-        self._min_speed_cmd = params["reward_params"]["min_speed_cmd"]
-        self._max_speed_cmd = params["reward_params"]["max_speed_cmd"]
+        self._comfort_max_steering = params["reward_params"]["comfort_max_steering"]
+        self._comfort_max_norm = params["reward_params"]["comfort_max_norm"]
+        self._min_velx = params["reward_params"]["min_velx"]
+        self._max_velx = params["reward_params"]["max_velx"]
+        self._limit_velx = params["action_config"]["max_velx"]
         self._max_steps = params["max_steps"]
+        self._frame_skip = params["frame_skip"]
         self._steps = 0
         self._initial_progress = None
-        self._n_last_actions = params["n_last_actions"]
-        self._last_actions = collections.deque([[0.0] * len(self.action_space)] * self._n_last_actions,
-                                               maxlen=self._n_last_actions)
 
         # extend observation space (we need them to compute the potential, the agent do not directly observe them)
         self.observation_space["progress"] = Box(low=0.0, high=1.0, dtype=np.float32, shape=(1,))
@@ -43,8 +45,6 @@ class RacecarEnv(ChangingTrackSingleAgentRaceEnv):
         self.observation_space["collision"] = Box(low=0.0, high=1.0, dtype=np.float32, shape=(1,))
         minvel, maxvel = self.observation_space["velocity"].low[0], self.observation_space["velocity"].high[0]
         self.observation_space["velocity_x"] = Box(low=minvel, high=maxvel, shape=(1,))
-        self.observation_space["last_actions"] = Box(low=-1, high=+1,
-                                                     shape=(self._n_last_actions, len(self.action_space)))
 
         self._eval = params["eval"]
         self._seed = params["seed"]
@@ -53,15 +53,32 @@ class RacecarEnv(ChangingTrackSingleAgentRaceEnv):
     @staticmethod
     def _get_params(**kwargs):
         params = {
-            "max_steps": 600,
+            "max_steps": 2500,
             "n_last_actions": 3,
             "reward_params": {
                 "target_progress": 0.99,
                 "target_dist2obst": 0.5,
-                "min_speed_cmd": -1.0,
-                "max_speed_cmd": -1.0,
+                "min_velx": 2.0,
+                "max_velx": 3.0,
+                "comfort_max_steering": 0.1,
+                "comfort_max_norm": 1.0,
             },
-            "render": False,
+            "action_config": {
+                "delta_speed": False,
+                "min_velx": 0.0,
+                "max_velx": +3.5,
+                "cap_min_velx": 0.0,
+                "cap_max_velx": +3.5,
+                "max_accx": 4.0,
+                "dt": 0.01,
+            },
+            "observation_config": {
+                "use_history_wrapper": False,
+                "n_last_actions": 1,
+                "n_last_observations": 1,
+            },
+            "frame_skip": 1,
+            "render": True,
             "eval": False,
             "seed": 0,
         }
@@ -75,22 +92,17 @@ class RacecarEnv(ChangingTrackSingleAgentRaceEnv):
 
     def reset(self):
         obs = super(RacecarEnv, self).reset(mode='grid' if self._eval else 'random')
-        # extend observations
-        self._last_actions = collections.deque([[0.0] * len(self.action_space)] * self._n_last_actions,
-                                               maxlen=self._n_last_actions)
         self._initial_progress = None
-        dummy_info = {"wall_collision": False, "progress": None, "obstacle": 1.0}
+        dummy_info = {"wall_collision": False, "progress": None, "obstacle": 1.0, "lap": 1}
         obs = self._extend_obs(obs, dummy_info)
         self._steps = 0
         return obs
 
     def step(self, action: Dict):
         obs, reward, done, info = super(RacecarEnv, self).step(action)
-        flat_action = np.array([action["steering"][0], action["speed"][0]])
-        self._last_actions.append(flat_action)
         self._steps += 1
-        obs = self._extend_obs(obs, info)
         info = self._extend_info(reward, done, info)
+        obs = self._extend_obs(obs, info)
         done = self._check_termination(obs, done, info)
         return obs, reward, done, info
 
@@ -98,28 +110,31 @@ class RacecarEnv(ChangingTrackSingleAgentRaceEnv):
         if self._initial_progress is None and info["progress"] is not None:
             # update the initial-progress on the first available progress after reset
             self._initial_progress = info["progress"]
-        progress = 0.0 if self._initial_progress is None else info["progress"] - self._initial_progress
+        progress = 0.0 if self._initial_progress is None else (info["lap"] - 1) + (info["progress"] - self._initial_progress)
         obs["collision"] = float(info["wall_collision"])
         obs["progress"] = progress
         obs["dist2obst"] = info["obstacle"]
         obs["velocity_x"] = np.array([obs["velocity"][0]], dtype=np.float32)
-        obs["last_actions"] = np.array(self._last_actions, dtype=np.float32)
         return obs
 
     def _extend_info(self, reward, done, info):
         info["default_reward"] = reward
         info["target_progress"] = self._target_progress
         info["target_dist2obst"] = self._target_dist2obst
-        info["min_speed_cmd"] = self._min_speed_cmd
-        info["max_speed_cmd"] = self._max_speed_cmd
+        info["comfort_max_steering"] = self._comfort_max_steering
+        info["comfort_max_norm"] = self._comfort_max_norm
+        info["min_velx"] = self._min_velx
+        info["max_velx"] = self._max_velx
+        info["limit_velx"] = self._limit_velx
         info["steps"] = self._steps
         info["max_steps"] = self._max_steps
+        info["frame_skip"] = self._frame_skip
         info["done"] = done
         return info
 
     def _check_termination(self, obs, done, info):
         collision = info["wall_collision"]
-        lap_completion = info["progress"] >= self._target_progress
+        lap_completion = obs["progress"] >= self._target_progress
         timeout = self._steps >= self._max_steps
         return bool(done or collision or lap_completion or timeout)
 
@@ -134,27 +149,39 @@ if __name__ == "__main__":
 
     scenario_files = ["treitlstrasse_single_agent.yml", "treitlstrasse_single_agent.yml"]
     scenario_files = ["treitlstrasse_single_agent.yml"]
-    action_config = {"min_speed": 1.0, "max_speed": 2.0, "max_accx": 4.0, "dt": 0.01}
+    action_config = {"min_velx": 0.0, "max_velx": 3.5,
+                     "cap_min_speed": 1.0, "cap_max_speed": 3.5,
+                     "max_accx": 4.0, "dt": 0.01}
 
-    env = RacecarEnv(scenario_files, render=True)
+    env = RacecarEnv(scenario_files, render=True, eval=True)
+    env = ActionHistoryWrapper(env, n_last_actions=3)
     env = DeltaSpeedWrapper(env, action_config=action_config, frame_skip=1)
+    env = ObservationHistoryWrapper(env, obs_name="lidar_64", n_last_obs=3)
 
     for _ in range(1):
 
         speeds = []
+        cmds = []
+        norms = []
 
         env.reset()
         done = False
         tot_reward = 0
+        steps = 0
         while not done:
             action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
             tot_reward += reward
-            speeds.append(env.speed_ms)
+            speeds.append(obs["velocity_x"])
+            cmds.append(env.speed_ms)
+            steps += 1
+            norms.append(np.linalg.norm(obs["last_actions"][-1] - obs["last_actions"][-2]))
         print(tot_reward)
 
     import matplotlib.pyplot as plt
     plt.plot(speeds)
+    plt.plot(cmds)
+    plt.plot(norms)
     plt.show()
 
     env.close()
--- reward_shaping/envs/wrappers.py
+++ reward_shaping/envs/wrappers.py
@@ -187,32 +187,4 @@ class FrameSkip(gym.Wrapper):
         return self.env.reset(**kwargs)
 
 
-class DeltaSpeedWrapper(gym.ActionWrapper):
-    def __init__(self, env, frame_skip: int, action_config: Dict[str, Any], **kwargs):
-        super(DeltaSpeedWrapper, self).__init__(env)
-        self.action_space = gym.spaces.Dict({
-            "delta_speed": Box(low=-1.0, high=1.0, dtype=np.float32, shape=(1,)),
-            "steering": Box(low=-1.0, high=1.0, dtype=np.float32, shape=(1,))
-        })
-        self._max_delta_speed = action_config["max_accx"] * frame_skip * action_config["dt"]
-        self._minspeed = action_config["min_speed"]
-        self._maxspeed = action_config["max_speed"]
 
-    def reset(self, **kwargs):
-        self.speed_ms = np.array([0.0], dtype=np.float32)  # last speed cmd in m/s
-        return super(DeltaSpeedWrapper, self).reset(**kwargs)
-
-    def action(self, action):
-        delta_speed = action["delta_speed"][0] * self._max_delta_speed     # ranges in +-max delta speed
-        self.speed_ms = self.speed_ms + delta_speed
-        self.speed_ms = np.clip(self.speed_ms, self._minspeed, self._maxspeed)
-
-        original_action = {
-            "speed": self.speed_ms,
-            "steering": action["steering"],
-        }
-        return original_action
-
-    def reverse_action(self, action):
-        raise NotImplementedError("delta speed wrapper reverse action")
-        pass
